352fa530ca9dfee64540165a3f7534520ee23800
Mirela <adisserdarevic@gmail.com>
2021-03-12 22:26:27 +0100

    Added file index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change code below this line
+    var lastName = "Doe";
+    var lastNameLenght;
+    lastNameLenght = lastName.length;
+    // Only change code above this line
+    return lastNameLenght;
+    
+}
+console.log(main());
+module.exports = main;
