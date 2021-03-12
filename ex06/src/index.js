de7a31e38c58e1cf2749a9165af2e4d66245b831
Mirela <adisserdarevic@gmail.com>
2021-03-12 22:17:55 +0100

    Added file index.js

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -2,7 +2,7 @@ function main() {
     // Only change code below this line
     var myName = "Paragon ";
     var myString = "My name is " + myName + ", and I am well!";
-    // myString  is now "My name is Paragon , and I am well!."
+    // myString is now "My name is Paragon , and I am well!."
     
     // Only change code above this line
     return myString;
