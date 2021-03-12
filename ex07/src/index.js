2894614abe58d3ccad238538b7e405a0a4ff2ca5
Mirela <adisserdarevic@gmail.com>
2021-03-12 22:07:52 +0100

    Added file index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+    var someAdjective = "the best!";
+    var myString = "Mistral is ";
+    myString += someAdjective;
+    // myString  is now "Mistral is the best."
+    
+    // Only change code above this line
+    return myString;
+    
+}
+console.log(main());
+module.exports = main;
