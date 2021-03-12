77f138bb4459e724ccb6d98f3e155044317226bf
Mirela <adisserdarevic@gmail.com>
2021-03-12 23:18:13 +0100

    Added file index.js

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change code below this line
+    var lastName = "Tesla";
+    var lastLetterOfLastName = lastName[lastName.length - 1];    //lastLetterOfLastName is "a"
+    // Only change code above this line
+    return lastLetterOfLastName;
+    
+}
+console.log(main());
+module.exports = main;
